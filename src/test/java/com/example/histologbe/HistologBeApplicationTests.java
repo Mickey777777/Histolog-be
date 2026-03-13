622fd64df11a86e86ac8d770cc27c9e75575ab92
Mickey777777 <wh879642@gmail.com>
2026-03-13 16:27:59 +0900

    init

---
+++ src/test/java/com/example/histologbe/HistologBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.histologbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistologBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
