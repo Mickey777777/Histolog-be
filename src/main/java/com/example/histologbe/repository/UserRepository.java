c08274b18e1f4aa5fa100b8fd48bd98b75ebaa40
Mickey777777 <wh879642@gmail.com>
2026-05-08 22:42:48 +0900

    feat: add token usage limit and reset schedule

--- src/main/java/com/example/histologbe/repository/UserRepository.java
+++ src/main/java/com/example/histologbe/repository/UserRepository.java
@@ -3,16 +3,23 @@ package com.example.histologbe.repository;
 import com.example.histologbe.domain.user.AuthProvider;
 import com.example.histologbe.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByUserId(UUID id);
     Optional<User> findByProviderId(String providerId);
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
 
     Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
+
+    @Modifying
+    @Query("UPDATE User u SET u.tokenUsage = 0")
+    void resetAllTokenUsage();
 }
